b49f175e1abda0091e61fa1cb1de1aa6dc1cc986
= <juliogalvis@example.com>
2024-10-14 15:42:01 -0500

    Ejercicios basicos hasta los numeros del 1 al 10 con for

--- Numeros1Al10/src/AppSerieNumeros.java
+++ Numeros1Al10/src/AppSerieNumeros.java
@@ -2,6 +2,13 @@ public class AppSerieNumeros {
     public static void main(String[] args) throws Exception {
         System.out.println("Numeros del 1 al 10");
 		int i = 1;
+		/*for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		do {
+			System.out.println(i);
+			i++;
+		}while(i <= 10);*/
 		while(i <= 10) {
 			System.out.println(i);
 			i++;
